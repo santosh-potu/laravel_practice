36744d80ddacd5c7529aa7b6bd76404f8c219890
Santosh <santoshreddy.potu@gmail.com>
2019-06-28 21:23:13 +0530

    Redirect

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -8,5 +8,7 @@ class RedirectController extends Controller
 {
     public function index() {
       echo "Redirecting to controller's action.";
+      //redirect()->action('RedirectController@index');
+      //redirect()->route('/get');
    }
 }
