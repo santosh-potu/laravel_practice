ae6beb25821eba2bc914b0634ad6c36f51e52999
Santosh <santoshreddy.potu@gmail.com>
2019-07-12 11:57:00 +0530

    implicit controller X

--- app/Http/Controllers/ImplicitController.php
+++ app/Http/Controllers/ImplicitController.php
@@ -19,5 +19,32 @@ class ImplicitController extends Controller
       dd($this->myclass);
    }
    
+   /**
+      * Responds to requests to GET /test
+   */
+   public function getIndex() {
+      echo 'index method';
+   }
+   
+   /**
+      * Responds to requests to GET /test/show/1
+   */
+   public function getShow($id) {
+      echo 'show method';
+   }
+   
+   /**
+      * Responds to requests to GET /test/admin-profile
+   */
+   public function getAdminProfile() {
+      echo 'admin profile method';
+   }
+   
+   /**
+      * Responds to requests to POST /test/profile
+   */
+   public function postProfile() {
+      echo 'profile method';
+   }
    
 }
